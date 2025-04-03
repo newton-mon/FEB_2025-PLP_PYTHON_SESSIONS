9a24b2be01cd2c08ebe47da2a61ac26e0005dc85
newton-mon <donaldnewton848.no@gmail.com>
2025-04-03 12:11:55 +0300

    update3

--- Week_3/List_Asignment.py
+++ Week_3/List_Asignment.py
@@ -1,31 +1,22 @@
-'''Create an empty list called my_list.
-Append the following elements to my_list: 10, 20, 30, 40.
-Insert the value 15 at the second position in the list.
-Extend my_list with another list: [50, 60, 70].
-Remove the last element from my_list.
-Sort my_list in ascending order.
-Find and print the index of the value 30 in my_list.'''
+'''Create a function named calculate_discount(price, discount_percent) 
+that calculates the final price after applying a discount. 
+The function should take the original price (price) and the discount 
+percentage (discount_percent) as parameters. If the discount is 20% or higher,
+apply the discount; otherwise, return the original price.
+ 
+Using the calculate_discount function, prompt the user to enter the 
+original price of an item and the discount percentage. Print the final 
+price after applying the discount, or if no discount was applied, print the original price.'''
 
-my_list = []
+def calclate_discount (a,b):
+    if b <= 20:
+        return (a)
+    else :
+        y= 1 - (b/100)
+        return(a*y)
 
-my_list.append(10)
-my_list.append(20)
-my_list.append(30)
-my_list.append(40)
-print(my_list)
 
-my_list.insert(1,15)
-print(my_list)
+price = int(input("What is the price: "))
+discount = int(input("What is the discount: "))
 
-ls_b=[60,50,70]
-
-my_list.extend(ls_b)
-print(my_list)
-
-my_list.remove(70)
-print(my_list)
-
-my_list.sort()
-print(my_list)
-
-print(my_list.index(30))
+print(calclate_discount(price,discount))
